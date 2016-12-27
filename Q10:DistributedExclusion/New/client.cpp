42988a76ffcf1fa9675f69438a8390a5c141221a
shoaib30 <shoaib30@gmail.com>
2016-12-27 16:37:20 +0530

    Added new program for Q10

---
+++ Q10:DistributedExclusion/New/client.cpp
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<iostream>
+#include<unistd.h>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<cstring>
+#include <arpa/inet.h>
+using namespace std;
+
+int main()
+{
+	int sockfd,cs;
+	socklen_t len;
+	struct sockaddr_in client;
+	char buf[50],buf1[50];
+	client.sin_family=AF_INET;
+	client.sin_addr.s_addr=inet_addr("127.0.0.1");
+	client.sin_port=htons(5005);
+	while(1){
+		sockfd=socket(AF_INET,SOCK_STREAM,0);
+		connect(sockfd,(struct sockaddr *)&client,sizeof(client));
+		cout<<"\nConnected to server\n";
+		recv(sockfd,buf1,50,0);
+		if(strcmp(buf1,"LOCKED")==0){
+			cout<<"Could not attain Mutual Exclusion\nStatus:"<<buf1<<endl;
+			cout<<"Going to sleep\n";
+			sleep(5);
+		}
+		else{
+			cout<<"Mutual Exlusion\nStatus: "<<buf1<<endl;
+			cout<<"Enter message: ";
+			fgets(buf,50,stdin);
+			send(sockfd,buf,50,0);
+			recv(sockfd,buf1,50,0);
+			cout<<"Message from server: "<<buf1;
+			break;
+		}
+		close(sockfd);
+	}
+	return 0;
+}
---
+++ Q10:DistributedExclusion/New/server.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+#include<vector>
+#include<cstdlib>
+#include<pthread.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<netinet/in.h>
+#include<sys/socket.h>
+#include<stdlib.h>
+
+
+using namespace std;
+
+struct thread_data{
+  int sockfd;
+  int cs;
+  bool status;
+};
+
+void *responseFunction(void *threaddata)  {
+  thread_data *data = (thread_data*) threaddata;
+  int sockfd = data->sockfd;
+  int cs = data->cs;
+  char  buf[50];
+  if(!data->status){
+    char  statusBuffer[50] = "UNLOCKED";
+    send(cs,statusBuffer,50,0);
+    data->status = true;
+    cout<<"Mutex: "<<"LOCKED\n";
+    cout<<"Client Connected\n";
+    recv(cs,buf,50,0);
+    cout<<"Message Received: "<<buf;
+    cout<<"Enter message to send: ";
+    fgets(buf,50,stdin);
+    send(cs,buf,50,0);
+    close(cs);
+    data->status = false;
+    cout<<"Mutex: "<<"UNLOCKED\n\n";
+  }else{
+    char  statusBuffer[50] = "LOCKED";
+    send(cs,statusBuffer,50,0);
+    close(cs);
+  }
+  pthread_exit(NULL);
+
+}
+
+int main()  {
+  int sockfd,cs;
+  socklen_t len;
+  bool status = false;
+	struct sockaddr_in server,client;
+  vector<pthread_t> threads;
+  sockfd=socket(AF_INET,SOCK_STREAM,0);
+	cout<<"Socket created sucessfully"<<endl;
+	server.sin_family=AF_INET;
+	server.sin_addr.s_addr=INADDR_ANY;
+	server.sin_port=htons(5005);
+	bind(sockfd,(sockaddr*)&server,sizeof(server));
+	cout<<"Binding done"<<endl;
+	listen(sockfd,5);
+  cout<<"listening\n\n";
+  thread_data td;
+  td.sockfd = sockfd;
+  td.status = status;
+  while(1)
+	{
+		len=sizeof(client);
+		cs=accept(sockfd,(struct sockaddr *)&client,&len);
+    td.cs = cs;
+    pthread_t temp;
+    pthread_create(&temp, NULL, responseFunction, (void *)&td);
+    threads.push_back(temp);
+  }
+  pthread_exit(NULL);
+}
